a8d7a6c26e3444cb9b27a1842d67bbe5d28599c7
nayandrakshapally <nayan.drakshapally@gmail.com>
2017-02-06 09:43:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-third', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-third.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
